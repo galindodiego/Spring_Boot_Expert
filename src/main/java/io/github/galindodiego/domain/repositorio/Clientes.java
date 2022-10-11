ca570593bafc63b4ec5e729c04c760a3ed401817
Diego Galindo <diegogalindo84@gmail.com>
2022-10-11 10:22:25 -0300

    Mapeando a entidade Produto

--- src/main/java/io/github/galindodiego/domain/repositorio/Clientes.java
+++ src/main/java/io/github/galindodiego/domain/repositorio/Clientes.java
@@ -2,13 +2,21 @@ package io.github.galindodiego.domain.repositorio;
 
 import io.github.galindodiego.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface Clientes extends JpaRepository<Cliente,Integer> {
 
 
-    List<Cliente> findByNomeLike(String nome);
 
     boolean existsByNome(String nome);
+
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ",nativeQuery = true)
+    List<Cliente> encontrarPorNome(@Param("nome") String nome);
+    @Modifying
+    @Query(" delete from Cliente c where c.nome =:nome ")
+    void deleteByNome(String nome);
 }
