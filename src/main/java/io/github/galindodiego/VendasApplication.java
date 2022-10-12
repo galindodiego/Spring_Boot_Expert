52d667410b845f5ec5c260a0f6a7baa856cfa515
Diego Galindo <diegogalindo84@gmail.com>
2022-10-12 08:35:55 -0300

    Criando o controller de clientes

--- src/main/java/io/github/galindodiego/VendasApplication.java
+++ src/main/java/io/github/galindodiego/VendasApplication.java
@@ -17,38 +17,6 @@ import java.util.List;
 @SpringBootApplication
 public class VendasApplication {
 
-
-    @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos)
-            {
-        return args -> {
-            System.out.println("Salvando Clientes");
-            Cliente diego = new Cliente("Diego");
-            clientes.save(diego);
-
-            Pedido p = new Pedido();
-            p.setCliente(diego);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-//            Cliente cliente =clientes.findClienteFetchPedidos(diego.getId());
-//            System.out.println(cliente);
-//            System.out.println(cliente.getPedidos());
-
-
-            pedidos.findByCliente(diego).forEach(System.out::println);
-
-//            List<Cliente> result = clientes.encontrarPorNome("Diego");
-//            result.forEach(System.out::println);
-
-        };
-
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class,args);
     }
