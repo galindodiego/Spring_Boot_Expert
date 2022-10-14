9cb194041106e0a6cbc9d39ad94cb5a82c142d61
Diego Galindo <diegogalindo84@gmail.com>
2022-10-14 18:25:23 -0300

    Utilizando Lombok

--- src/main/java/io/github/galindodiego/domain/entity/Produto.java
+++ src/main/java/io/github/galindodiego/domain/entity/Produto.java
@@ -1,10 +1,16 @@
 package io.github.galindodiego.domain.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 @Table(name = "produto")
+
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,27 +21,4 @@ public class Produto {
     @Column(name="preco_unitario")
     private BigDecimal preco;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
 }
--- src/main/java/io/github/galindodiego/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/galindodiego/rest/dto/PedidoDTO.java
@@ -1,8 +1,14 @@
 package io.github.galindodiego.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.List;
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PedidoDTO {
     private Integer cliente;
     private BigDecimal total;
