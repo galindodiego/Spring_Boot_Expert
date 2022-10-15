532c874f41086f1b30a48e05437f21f5f8ec2b4a
Diego Galindo <diegogalindo84@gmail.com>
2022-10-15 13:11:27 -0300

    Atualizado para gerar status do pedido

--- src/main/java/io/github/galindodiego/rest/controller/PedidoController.java
+++ src/main/java/io/github/galindodiego/rest/controller/PedidoController.java
@@ -1,10 +1,20 @@
 package io.github.galindodiego.rest.controller;
 
+import io.github.galindodiego.domain.entity.ItemPedido;
 import io.github.galindodiego.domain.entity.Pedido;
+import io.github.galindodiego.rest.dto.InformacaoItemPedidoDTO;
+import io.github.galindodiego.rest.dto.InformacoesPedidoDTO;
 import io.github.galindodiego.rest.dto.PedidoDTO;
 import io.github.galindodiego.service.PedidoService;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -23,5 +33,46 @@ public class PedidoController {
        return pedido.getId();
     }
 
+    @GetMapping("{id}")
+    public InformacoesPedidoDTO getById(@PathVariable Integer id){
+        return service
+                .obterPedidoCompleto(id)
+                .map(p-> converter(p) )
+                .orElseThrow(()->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,"Pedido não encontrado. "));
+
+
+    }
+    private InformacoesPedidoDTO converter(Pedido pedido){
+       return InformacoesPedidoDTO
+                .builder()
+                .codigo(pedido.getId())
+                .dataPedido(pedido.getDataPedido().format(DateTimeFormatter.ofPattern("dd/MM/YYYY")))
+                .cpf(pedido.getCliente().getCpf())
+                .nomeCliente(pedido.getCliente().getNome())
+                .total(pedido.getTotal())
+                .items(converter(pedido.getItens()))
+                .build();
+
+
+    }
+
+    private List<InformacaoItemPedidoDTO> converter (List<ItemPedido> itens){
+        if(CollectionUtils.isEmpty(itens)){
+            return Collections.emptyList();
+        }
+
+        return itens.stream().map(
+                item -> InformacaoItemPedidoDTO
+                        .builder().descricaoProduto(item.getProduto().getDescricao())
+                        .precoUnitario(item.getProduto().getPreco())
+                        .quantidade(item.getQuantidade())
+                        .build())
+                .collect(Collectors.toList());
+
+
+
+
+    }
 
 }
--- src/main/java/io/github/galindodiego/service/PedidoService.java
+++ src/main/java/io/github/galindodiego/service/PedidoService.java
@@ -3,7 +3,11 @@ package io.github.galindodiego.service;
 import io.github.galindodiego.domain.entity.Pedido;
 import io.github.galindodiego.rest.dto.PedidoDTO;
 
+import java.util.Optional;
+
 public interface PedidoService {
 
     Pedido salvar (PedidoDTO dto);
+
+    Optional <Pedido> obterPedidoCompleto(Integer id);
 }
