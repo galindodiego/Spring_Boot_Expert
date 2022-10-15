63c8b877578439b7cd5c83a3a2174758ddc76502
Diego Galindo <diegogalindo84@gmail.com>
2022-10-15 10:36:36 -0300

    Método para realizacao de um pedido

---
+++ src/main/java/io/github/galindodiego/exception/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package io.github.galindodiego.exception;
+
+public class RegraNegocioException extends RuntimeException {
+
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
--- src/main/java/io/github/galindodiego/service/PedidoService.java
+++ src/main/java/io/github/galindodiego/service/PedidoService.java
@@ -1,4 +1,9 @@
 package io.github.galindodiego.service;
 
+import io.github.galindodiego.domain.entity.Pedido;
+import io.github.galindodiego.rest.dto.PedidoDTO;
+
 public interface PedidoService {
+
+    Pedido salvar (PedidoDTO dto);
 }
